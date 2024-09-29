54da3e6f2b71cccf74671b3b327b767e0028830b
Arham Hussain 🥈 <153752282+ARHAM5432@users.noreply.github.com>
2024-09-29 07:31:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var nums = [1, 2, 5, 6, 7, 8, 9];
+var mNum = [];
+
+var min = nums[0];
+var max = nums[0];
+
+for (var i = 1; i < nums.length; i++) {
+    if (nums[i] < min) {
+        min = nums[i];
+    }
+    console.log("Chhoti value:", min);
+
+    if (nums[i] > max) {
+        max = nums[i];
+    }
+}
+
+for (var v = min; v <= max; v++) {
+    var present = false;
+    for (var x = 0; x < nums.length; x++) {
+        if (nums[x] === v) {
+            present = true;
+            break;
+        }
+    }
+    if (!present) {
+        mNum.push(v);
+    }
+}
+
+alert("Missing numbers: ");
+for (var a = 0; m < mNum.length; a++) {
+    alert(mNum[a] + " ");
+}
